330afe91ec47bd3e47e51fcd15a38c0f971dd7d3
luuthuong <luuthuong20022000@gmail.com>
2022-02-13 11:59:16 +0700

    initial commit

---
+++ Script.js
@@ -0,0 +1,7 @@
+// import $ from 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+import $ from "jquery";
+$(document).ready(function(){
+    $('.list-menu').click(()=>{
+        $('span').css('color', 'red');
+    })
+})
